c746a826311beb9598ffaf37ed15b2b244d0b066
Jon Betts <jon.betts@hypothes.is>
2020-11-13 14:38:41 +0000

    Add a family of classes for reading and using the blocklist
    This is heavily inspired by ViaHTML but with two major changes:
    
     * Externalising the enums and adding severities to them
     * Changing the cardinality to be potentially many reasons instead of one

---
+++ checkmate/checker/url/reason.py
@@ -0,0 +1,78 @@
+"""A collection of reasons URLs can be blocked (and friends)."""
+
+from enum import Enum
+from functools import total_ordering
+
+
+@total_ordering
+class OrderedEnum(Enum):
+    """Enumerations which compare and sort in the order they are defined."""
+
+    def __lt__(self, other):
+        if not isinstance(other, type(self)):
+            raise TypeError("Can only compare an enum with another instance of itself")
+
+        if self == other:
+            return False
+
+        for item in type(self).__members__.values():
+            if item == self:
+                return True
+            if item == other:
+                return False
+
+        # Shouldn't really be possible
+        return False  # pragma: no cover
+
+
+class Severity(OrderedEnum):
+    """List of level of severity of a block."""
+
+    # These items can be blocked, or not, at the discretion of the service
+    ADVISORY = "advisory"
+    # These items must be blocked by all services
+    MANDATORY = "mandatory"
+
+
+class Reason(Enum):
+    """List of reasons a URL can be blocked."""
+
+    MALICIOUS = "malicious"  # Actively hostile content of some kind
+    PUBLISHER_BLOCKED = "publisher-blocked"  # Content owner has asked us to block
+    MEDIA_VIDEO = "media-video"  # Sites which are mostly video content
+    MEDIA_AUDIO = "media-audio"  # Sites which are mostly audio content
+    MEDIA_IMAGE = "media-image"  # Sites which are mostly image content
+    MEDIA_MIXED = "media-mixed"  # Sites with a mixture of content
+    HIGH_IO = "high-io"  # Sites with high interactivity and AJAX calls
+    OTHER = "other"  # Escape hatch for poorly formatted values
+
+    @property
+    def severity(self):
+        """Get the severity of this reason."""
+
+        if self in (self.MALICIOUS, self.PUBLISHER_BLOCKED):
+            return Severity.MANDATORY
+
+        return Severity.ADVISORY
+
+    @classmethod
+    def parse(cls, value):
+        """Parse a value into an enum object."""
+        try:
+            return cls(value)
+        except ValueError:
+            return cls.OTHER
+
+    def serialise(self):
+        """Convert to a JSON API resource object."""
+
+        # https://jsonapi.org/format/#document-resource-objects
+        return {
+            "type": "reason",
+            "id": self.value,
+            "attributes": {
+                # pylint: disable=no-member
+                # pylint is just confused... not sure why
+                "severity": self.severity.value
+            },
+        }
---
+++ tests/unit/checkmate/checker/url/blocklist_test.py
@@ -0,0 +1,122 @@
+import os
+
+import pytest
+from h_matchers import Any
+
+from checkmate.checker.url.blocklist import Blocklist
+from checkmate.checker.url.reason import Reason
+
+
+class TestBlocklist:
+    def test_it_can_start_without_a_file(self):
+        blocklist = Blocklist("missing.txt")
+
+        assert blocklist.domains == {}
+
+    def test_it_reloads_when_the_file_changes(self, blocklist_file):
+        blocklist = Blocklist(blocklist_file)
+        assert blocklist.domains  # Check we've loaded something
+        blocklist.clear()
+        blocklist_file.touch()  # Reset the access times
+
+        list(blocklist.check_url("foo"))
+
+        # The original set has not been reloaded
+        # assert blocklist.domains
+
+    def test_it_does_not_reload_if_the_file_is_the_same(self, blocklist_file):
+        blocklist = Blocklist(blocklist_file)
+        assert blocklist.domains  # Check we've loaded something
+        blocklist.clear()
+
+        list(blocklist.check_url("foo"))
+
+        # The original set has not been reloaded
+        assert blocklist.domains == {}
+
+    @pytest.mark.parametrize(
+        "line,reason",
+        (
+            ("example.com publisher-blocked", Reason.PUBLISHER_BLOCKED),
+            ("example.com malicious", Reason.MALICIOUS),
+            ("   example.com    media-mixed   ", Reason.MEDIA_MIXED),
+            (
+                "example.com media-image   # trailing comment",
+                Reason.MEDIA_IMAGE,
+            ),
+            ("example.com other", Reason.OTHER),
+            ("example.com right-format-wrong-value", Reason.OTHER),
+            # Comment
+            ("# any old comment", None),
+            # Unparsable
+            ("example.com", None),
+            ("example.com too many parts", None),
+            # Allowed for viahtml
+            ("example.com  media-video", None),
+        ),
+    )
+    def test_file_loading(self, tmp_path, line, reason):
+        filename = tmp_path / "blocklist.txt"
+        filename.write_text(line)
+
+        blocklist = Blocklist(filename)
+
+        assert blocklist.domains == ({"example.com": reason} if reason else {})
+
+    @pytest.mark.parametrize(
+        "line,reason",
+        (
+            ("*.example.com publisher-blocked", Reason.PUBLISHER_BLOCKED),
+            ("*.example.com rubbish", Reason.OTHER),
+            ("*.example.com", None),
+        ),
+    )
+    def test_file_loading_wildcards(self, tmp_path, line, reason):
+        filename = tmp_path / "blocklist.txt"
+        filename.write_text(line)
+
+        blocklist = Blocklist(filename)
+
+        if not reason:
+            assert blocklist.patterns == {}
+        else:
+            assert len(blocklist.patterns) == 1
+            ((domain, found_reason),) = blocklist.patterns.items()
+            assert found_reason == reason
+            # This regex happens to be what `fnmatch.translate` spits out
+            assert domain.pattern == r"(?s:.*\.example\.com)\Z"
+
+    @pytest.mark.parametrize(
+        "url,reasons",
+        (
+            ("https://www.example.com", [Reason.MALICIOUS]),
+            ("http://www.example.com", [Reason.MALICIOUS]),
+            ("//www.example.com", [Reason.MALICIOUS]),
+            ("www.example.com", [Reason.MALICIOUS]),
+            ("http://www.example.com/path", [Reason.MALICIOUS]),
+            ("http://www.example.com/path?a=b", [Reason.MALICIOUS]),
+            # Sub-domains don't count
+            ("http://example.com", []),
+            ("http://example.org", []),
+            # Wildcard matching
+            ("anything.example.net", [Reason.OTHER]),
+            ("anything.nested.example.net", [Reason.OTHER]),
+            ("thisisfineexample.net", []),
+        ),
+    )
+    def test_url_matching(self, url, reasons):
+        blocklist = Blocklist("missing.txt")
+        blocklist.add_domain("www.example.com", Reason.MALICIOUS)
+        blocklist.add_domain("*.example.net", Reason.OTHER)
+
+        assert blocklist.check_url(url) == Any.generator.containing(reasons).only()
+
+    @pytest.fixture
+    def blocklist_file(self, tmp_path):
+        blocklist_file = tmp_path / "blocklist.txt"
+        blocklist_file.write_text("example.com other")
+
+        # Make the file very old
+        os.utime(blocklist_file, (0, 0))
+
+        return blocklist_file
