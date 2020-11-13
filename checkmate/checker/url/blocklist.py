bf19646ba6aa5730034a6bc15428ac8206236857
Jon Betts <jon.betts@hypothes.is>
2020-11-13 14:38:41 +0000

    Add a family of classes for reading and using the blocklist
    This is heavily inspired by ViaHTML but with two major changes:
    
     * Externalising the enums and adding severities to them
     * Changing the cardinality to be potentially many reasons instead of one

---
+++ checkmate/checker/url/blocklist.py
@@ -0,0 +1,122 @@
+"""The abstract blocklist object."""
+
+import fnmatch
+import os
+import re
+from logging import getLogger
+from urllib.parse import urlparse
+
+from checkmate.checker.url.reason import Reason
+
+
+class Blocklist:
+    """A blocklist which URLs can be checked against.
+
+    For details of how to change the blocklist see:
+      * https://stackoverflow.com/c/hypothesis/questions/102/250
+
+    And is downloaded locally, via supervisor using `bin/fetch-blocklist`
+    """
+
+    LOG = getLogger(__name__)
+
+    # viahtml is ok with video, as far as we can tell
+    PERMITTED = (Reason.MEDIA_VIDEO,)
+
+    def __init__(self, filename):
+        self.LOG.debug("Monitoring blocklist file '%s'", filename)
+
+        self._filename = filename
+        self._last_modified = None
+        self.domains = {}
+        self.patterns = {}
+
+        self._refresh()
+
+    def check_url(self, url):
+        """Test the URL and return a list of reasons it should be blocked.
+
+        :param url: URL to test
+        :return: An iterable of Reason objects (which may be empty)
+        """
+        self._refresh()
+
+        domain = self._domain(url)
+        blocked = self.domains.get(domain)
+        if blocked:
+            yield blocked
+
+        for pattern, reason in self.patterns.items():
+            if pattern.match(domain):
+                yield reason
+
+    def clear(self):
+        """Remove all domains from the blocklist."""
+        self.domains, self.patterns = {}, {}
+
+    def add_domain(self, domain, reason):
+        """Add a domain (or domain pattern) to the blocklist."""
+
+        reason = Reason.parse(reason)
+        if reason in self.PERMITTED:
+            # This is listed as blocked, but this service can actually
+            # serve this type without incident
+            return
+
+        if "*" in domain:
+            # Convert a string with '*' wildcards into a regex
+            pattern = re.compile(fnmatch.translate(domain), re.IGNORECASE)
+            self.patterns[pattern] = reason
+        else:
+            self.domains[domain] = reason
+
+    @classmethod
+    def _domain(cls, url):
+        parsed_url = urlparse(url)
+
+        if not parsed_url.scheme:
+            parsed_url = urlparse(f"http://{url.lstrip('/')}")
+
+        return parsed_url.hostname
+
+    def _refresh(self):
+        if self._file_changed:
+            self.LOG.debug("Reloading blocklist file")
+
+            self.clear()
+            for domain, reason in self._parse(self._filename):
+                self.add_domain(domain, reason)
+
+    @property
+    def _file_changed(self):
+        if not os.path.exists(self._filename):
+            self.LOG.warning("Cannot find blocklist file '%s'", self._filename)
+            return False
+
+        last_modified = os.stat(self._filename).st_mtime
+        if last_modified != self._last_modified:
+            self._last_modified = last_modified
+            return True
+
+        return False
+
+    LINE_PATTERN = re.compile(r"^(\S+)\s+(\S+)(?:\s*#.*)?$")
+
+    @classmethod
+    def _parse(cls, filename):
+        with open(filename) as handle:
+            for line in handle:
+                line = line.strip()
+
+                if not line or line.startswith("#"):
+                    # Empty or comment line.
+                    continue
+
+                match = cls.LINE_PATTERN.match(line)
+                if match:
+                    domain, reason = match.group(1), match.group(2)
+                else:
+                    cls.LOG.warning("Cannot parse blocklist file line: '%s'")
+                    continue
+
+                yield domain, reason
---
+++ tests/unit/checkmate/checker/url/reason_test.py
@@ -0,0 +1,68 @@
+import operator
+
+import pytest
+
+from checkmate.checker.url.reason import OrderedEnum, Reason, Severity
+
+
+class TestOrderedEnum:
+    class DummyEnum(OrderedEnum):
+        """Enum for testing."""
+
+        FIRST = 2
+        MIDDLE = 3
+        LAST = 1
+
+    @pytest.mark.parametrize("l_pos,l_value", enumerate(DummyEnum))
+    @pytest.mark.parametrize("r_pos,r_value", enumerate(DummyEnum))
+    @pytest.mark.parametrize(
+        "op",
+        [operator.lt, operator.le, operator.eq, operator.ne, operator.ge, operator.gt],
+    )
+    # pylint: disable=too-many-arguments
+    def test_comparison(self, l_pos, l_value, op, r_pos, r_value):
+        assert op(l_value, r_value) is op(l_pos, r_pos)
+
+    def test_it_fails_on_comparison_with_non_member(self):
+        with pytest.raises(TypeError):
+            assert self.DummyEnum.FIRST < 3
+
+
+class TestSeverity:
+    def test_it_sorts_correctly(self):
+        assert Severity.ADVISORY < Severity.MANDATORY
+
+
+class TestReason:
+    @pytest.mark.parametrize(
+        "value,reason",
+        (
+            ("malicious", Reason.MALICIOUS),
+            ("publisher-blocked", Reason.PUBLISHER_BLOCKED),
+            ("not a value", Reason.OTHER),
+            (None, Reason.OTHER),
+        ),
+    )
+    def test_it_can_parse(self, value, reason):
+        assert Reason.parse(value) == reason
+
+    @pytest.mark.parametrize(
+        "reason,severity",
+        (
+            (Reason.MALICIOUS, Severity.MANDATORY),
+            (Reason.PUBLISHER_BLOCKED, Severity.MANDATORY),
+            (Reason.MEDIA_AUDIO, Severity.ADVISORY),
+            (Reason.OTHER, Severity.ADVISORY),
+        ),
+    )
+    def test_severity(self, reason, severity):
+        assert reason.severity == severity
+
+    def test_serialise(self):
+        assert Reason.MALICIOUS.serialise() == {
+            "type": "reason",
+            "id": "malicious",
+            "attributes": {
+                "severity": "mandatory",
+            },
+        }
