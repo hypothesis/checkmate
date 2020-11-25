32ab3beac9205f93653eb2d94082340d4b99e173
Jon Betts <jon.betts@hypothes.is>
2020-11-25 15:18:02 +0000

    Add the requirements and local dev services for postgres and celery

--- requirements/compile.sh
+++ requirements/compile.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
+# Get around the year zero problem of not being able to create requirements
+# files because there are no requirements files for the tox envs.
+touch requirements/requirements.txt
+touch requirements/dev.txt
+touch requirements/format.txt
+touch requirements/tests.txt
+touch requirements/lint.txt
+touch requirements/dockercompose.txt
+
 # No dependencies
 tox -e dev --run-command "pip-compile requirements/requirements.in"
 tox -e format --run-command "pip-compile requirements/format.in"
+tox -e dockercompose --run-command "pip-compile requirements/dockercompose.in"
 
 # Depends on requirements.txt
 tox -e dev --run-command "pip-compile requirements/dev.in"
