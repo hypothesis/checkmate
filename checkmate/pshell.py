5e69745475348f12e436b4c02492d5197f5527fa
Sean Hammond <seanh@users.noreply.github.com>
2020-12-09 12:53:21 +0000

    Add `make shell`
    The same as our other apps have, launches an iPython shell in the dev
    virtualenv. This is very handy:
    
        $ make shell
        Python 3.6.9 (default, Dec  6 2020, 21:12:12)
        Type 'copyright', 'credits' or 'license' for more information
        IPython 7.16.1 -- An enhanced Interactive Python. Type '?' for help.
    
        Environment:
        app          The WSGI application.
        db           Active DB session.
        f            The test factories for quickly creating objects.
        factories    The test factories for quickly creating objects.
        m            The checkmate.models package.
        models       The checkmate.models package.
        registry     Active Pyramid registry.
        request      Active request object.
        root         Root of the default resource tree.
        root_factory Default root factory used to create `root`.
        session      Active DB session.
        tm           Active transaction manager (a transaction is already begun).
    
        In [1]: db.query(m.CustomRule).first()
        Out[1]: CustomRule(id=97, rule='nautil.us/', hash='05d4a5a43fbb107f5b086b08608e474144f1a70a9a7c62041b1fb57d437c2dbf', tags=['publisher-blocked'])
    
        In [2]: factories.CustomRule()
        Out[2]: CustomRule(id=None, rule='www.smith.com/', hash='86a1a55d4647694a221d4d50a271cd270b2276bb7dddd8dea228d58f30b83a36', tags=['high-io'])

---
+++ checkmate/pshell.py
@@ -0,0 +1,37 @@
+import sys
+from contextlib import suppress
+
+from transaction.interfaces import NoTransaction
+
+from checkmate import models
+
+
+def setup(env):
+    sys.path = ["."] + sys.path
+
+    from tests import factories  # pylint:disable=import-outside-toplevel
+
+    sys.path = sys.path[1:]
+
+    request = env["request"]
+
+    request.tm.begin()
+
+    env["tm"] = request.tm
+    env["tm"].__doc__ = "Active transaction manager (a transaction is already begun)."
+
+    env["db"] = env["session"] = request.db
+    env["db"].__doc__ = "Active DB session."
+
+    env["m"] = env["models"] = models
+    env["m"].__doc__ = "The checkmate.models package."
+
+    env["f"] = env["factories"] = factories
+    env["f"].__doc__ = "The test factories for quickly creating objects."
+    factories.set_sqlalchemy_session(request.db)
+
+    try:
+        yield
+    finally:
+        with suppress(NoTransaction):
+            request.tm.abort()
