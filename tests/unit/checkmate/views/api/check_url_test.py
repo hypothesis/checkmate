060e8985a2929f2cef1a39ccc972361ea92df90e
Jon Betts <jon.betts@hypothes.is>
2020-12-11 15:48:27 +0000

    Integrate URLHaus into the responses and fix request.db fixture

--- tests/unit/checkmate/views/api/check_url_test.py
+++ tests/unit/checkmate/views/api/check_url_test.py
@@ -7,18 +7,29 @@ from checkmate.views.api.check_url import check_url
 
 
 class TestURLCheck:
-    def test_a_good_url(self, make_request):
+    def test_a_good_url(self, make_request, CustomRules, URLHaus):
         request = make_request("/api/check", {"url": "http://happy.example.com"})
 
-        result = check_url(request)
+        response = check_url(request)
 
-        assert result.status_code == 204
+        assert response.status_code == 204
 
-    def test_a_bad_url(self, make_request, CustomRules):
-        CustomRules.return_value.check_url.return_value = (
-            Reason.MEDIA_IMAGE,
-            Reason.MALICIOUS,
+        CustomRules.assert_called_once_with(request.db)
+        custom_rules = CustomRules.return_value
+        custom_rules.check_url.assert_called_once_with(
+            list(hash_url("http://happy.example.com"))
         )
+
+        URLHaus.assert_called_once_with(request.db)
+        url_haus = CustomRules.return_value
+        url_haus.check_url.assert_called_once_with(
+            list(hash_url("http://happy.example.com"))
+        )
+
+    def test_a_bad_url(self, make_request, CustomRules, URLHaus):
+        CustomRules.return_value.check_url.return_value = (Reason.MEDIA_IMAGE,)
+        URLHaus.return_value.check_url.return_value = (Reason.MALICIOUS,)
+
         request = make_request("/api/check", {"url": "http://sad.example.com"})
 
         result = check_url(request)
@@ -34,12 +45,6 @@ class TestURLCheck:
             },
         }
 
-        CustomRules.assert_called_once_with(request.db)
-        custom_rules = CustomRules.return_value
-        custom_rules.check_url.assert_called_once_with(
-            list(hash_url("http://sad.example.com"))
-        )
-
     def test_it_returns_an_error_for_no_url(self, make_request):
         request = make_request("/api/check")
 
@@ -49,5 +54,11 @@ class TestURLCheck:
     @pytest.fixture(autouse=True)
     def CustomRules(self, patch):
         CustomRules = patch("checkmate.views.api.check_url.CustomRules")
+        CustomRules.return_value.check_url.return_value = tuple()
+        return CustomRules
+
+    @pytest.fixture(autouse=True)
+    def URLHaus(self, patch):
+        CustomRules = patch("checkmate.views.api.check_url.URLHaus")
         CustomRules.return_value.check_url.return_value = tuple()
         return CustomRules
