f8f6431a25723a3848b0d9cefea73823dfcd2f46
Jon Betts <jon.betts@hypothes.is>
2020-12-16 13:42:28 +0000

    Add an exception specifically for timeouts in stages

--- checkmate/checker/pipeline/web.py
+++ checkmate/checker/pipeline/web.py
@@ -1,10 +1,10 @@
 """Stages which deal with online sources."""
 
 import requests
-from requests import RequestException
+from requests.exceptions import ReadTimeout, RequestException
 
 from checkmate.checker.pipeline.core import Stage
-from checkmate.exceptions import StageException
+from checkmate.exceptions import StageException, StageTimeoutException
 
 
 class Download(Stage):
@@ -12,23 +12,34 @@ class Download(Stage):
 
     CHUNK_SIZE = 64000
 
-    def __init__(self, url):
+    def __init__(self, url, timeout=10):
+        """Initialise a stage to download a file.
+
+        :param url: URL to retrieve
+        :param timeout: Maximum time to wait on getting it
+        :raise StageTimeoutException: If the request takes too long
+        :raise StageException: For any other problems
+        """
         self._url = url
+        self._timeout = timeout
 
     def __call__(self, working_dir, source=None):
         try:
             return self._download(self._url, working_dir)
+        except ReadTimeout as err:
+            raise StageTimeoutException(
+                f"Could not download url {self._url}: Timeout after {self._timeout}"
+            ) from err
         except RequestException as err:
             raise StageException(f"Could not download url {self._url}: {err}") from err
 
-    @classmethod
-    def _download(cls, url, working_dir):
-        with requests.get(url, timeout=10) as response:
+    def _download(self, url, working_dir):
+        with requests.get(url, timeout=self._timeout) as response:
             response.raise_for_status()
 
-            temp_file = cls.temp_file(working_dir, "zip")
+            temp_file = self.temp_file(working_dir, "zip")
 
-            for chunk in response.iter_content(cls.CHUNK_SIZE):
+            for chunk in response.iter_content(self.CHUNK_SIZE):
                 temp_file.write(chunk)
 
             return temp_file.name
