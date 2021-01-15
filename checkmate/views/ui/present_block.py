94492da48c8e373eaaf39f9a1139822ef75c8df5
Jon Betts <jon.betts@hypothes.is>
2021-01-15 18:49:07 +0000

    Add special wording for non-explictly allowed sites
    Also only prompt for feedback on those sites.

--- checkmate/views/ui/present_block.py
+++ checkmate/views/ui/present_block.py
@@ -1,4 +1,5 @@
 """User feedback for blocked pages."""
+from urllib.parse import urlparse
 
 from pyramid.exceptions import HTTPForbidden
 from pyramid.view import view_config
@@ -23,4 +24,10 @@ def present_block(_context, request):
 
     # At this point we know the contents of the args came from us, so we'll
     # assume that they are correct.
-    return {"blocked_url": request.GET["url"], "reason": request.GET["reason"]}
+    url_to_annotate = request.GET["url"]
+
+    return {
+        "blocked_url": url_to_annotate,
+        "domain_to_annotate": urlparse(url_to_annotate).netloc,
+        "reason": request.GET["reason"],
+    }
