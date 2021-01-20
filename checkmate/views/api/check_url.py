463655ae76174541b4f83fdf76decbd88ec38d0d
Jon Betts <jon.betts@hypothes.is>
2021-01-20 13:27:15 +0000

    Make the Check URL service emit signed URLs to the HTML block page

--- checkmate/views/api/check_url.py
+++ checkmate/views/api/check_url.py
@@ -5,6 +5,7 @@ from pyramid.view import view_config
 
 from checkmate.checker.url import CompoundRules
 from checkmate.exceptions import BadURLParameter
+from checkmate.services import SecureLinkService
 
 
 @view_config(route_name="check_url", renderer="json")
@@ -23,11 +24,19 @@ def check_url(request):
         # If everything is fine give a 204 which is successful, but has no body
         return HTTPNoContent()
 
+    # Reasons are in severity order, worst first
+    worst_reason = reasons[0]
+
     # https://jsonapi.org/format/#document-top-level
     return {
         "data": [reason.serialise() for reason in reasons],
         "meta": {
             # Reasons are in severity order, worst first
-            "maxSeverity": reasons[0].severity.value,
+            "maxSeverity": worst_reason.severity.value,
+        },
+        "links": {
+            "html": request.find_service(SecureLinkService).route_url(
+                "present_block", _query={"url": url, "reason": worst_reason.value}
+            )
         },
     }
