3948b31463cda490c6a10768bfafbb6c64f852dc
Jon Betts <jon.betts@hypothes.is>
2021-01-26 18:05:15 +0000

    Functionally group the app configuration
    Previously there was no rhyme or reason to where things happened
    now they are functionally grouped, so for example all "DB" config
    happens in one method

--- checkmate/app.py
+++ checkmate/app.py
@@ -5,106 +5,91 @@ import pyramid.config
 import pyramid_tm
 
 
-def load_settings(settings, celery_worker=False):
-    """Load application settings from a dict or environment variables.
+class CheckmateConfigurator:
+    def __init__(self, config, celery_worker=False):
+        self.config = config
+        self.celery_worker = celery_worker
 
-    Checks that the required parameters are either filled out in the provided
-    dict, or that the required values can be loaded from the environment.
+        self._configure_logging(config)
+        self._configure_db(config)
+        self._configure_sentry(config)
+        self._configure_checkmate(config)
 
-    :param settings: Settings dict
-    :param celery_worker: Only do the configuration strictly required for
-        celery workers
-    :raise ValueError: If a required parameter is not filled
-    :return: A dict of settings
-    """
-
-    required_params = ["database_url"]
-    if celery_worker:
-        required_params.append("checkmate_blocklist_url")
-
-    for param in required_params:
-        value = settings[param] = settings.get(param, os.environ.get(param.upper()))
-
+    def add_setting_from_env(self, param_name):
+        value = self.config.registry.settings.get(param_name) or os.environ.get(
+            param_name.upper()
+        )
         if value is None:
-            raise ValueError(f"Param {param} must be provided.")
+            raise ValueError(f"Param {param_name} must be provided.")
 
-    # Configure sentry
-    settings["h_pyramid_sentry.filters"] = []
-    if celery_worker:
-        settings["h_pyramid_sentry.celery_support"] = True
+        self.config.add_settings({param_name: value})
 
-    return settings
+    def _configure_checkmate(self, config):
+        if self.celery_worker:
+            self.add_setting_from_env("checkmate_blocklist_url")
+        else:
+            # The celery workers don't need to know about this stuff
 
+            # Include the secret for crytographic functions
+            self.add_setting_from_env("checkmate_secret")
 
-def configure(config, celery_worker=False):  # pragma: no cover
-    """Set up a pyramid config object for checkmate.
+            config.include("pyramid_services")
+            config.include("checkmate.services")
 
-    :param config: Pyramid configuration object to setup
-    :param celery_worker: Only do the configuration strictly required for
-        celery workers
-    """
+            config.include("pyramid_jinja2")
+            config.scan("checkmate.views")
+            config.include("checkmate.routes")
 
-    # Make sure that pyramid_exclog's tween runs under pyramid_tm's tween so
-    # that pyramid_exclog doesn't re-open the DB session after pyramid_tm has
-    # already closed it.
-    config.add_tween(
-        "pyramid_exclog.exclog_tween_factory", under="pyramid_tm.tm_tween_factory"
-    )
-    config.add_settings({"exclog.extra_info": True})
-    config.include("pyramid_exclog")
+        # pyramid-sanity should be activated as late as possible
+        config.include("pyramid_sanity")
 
-    # Use pyramid_tm's explicit transaction manager.
-    #
-    # This means that trying to access a request's transaction after pyramid_tm
-    # has closed the request's transaction will crash, rather than implicitly
-    # opening a new transaction that doesn't get closed (and potentially
-    # leaking open DB connections).
-    #
-    # This is recommended in the pyramid_tm docs:
-    #
-    # https://docs.pylonsproject.org/projects/pyramid_tm/en/latest/#custom-transaction-managers
-    config.registry.settings["tm.manager_hook"] = pyramid_tm.explicit_manager
-    config.include("pyramid_tm")
+    @classmethod
+    def _configure_logging(cls, config):
+        # Make sure that pyramid_exclog's tween runs under pyramid_tm's tween
+        # so that pyramid_exclog doesn't re-open the DB session after
+        # pyramid_tm has already closed it.
+        config.add_tween(
+            "pyramid_exclog.exclog_tween_factory", under="pyramid_tm.tm_tween_factory"
+        )
 
-    if celery_worker:
+        config.add_settings({"exclog.extra_info": True})
+        config.include("pyramid_exclog")
+
+    def _configure_db(self, config):
+        self.add_setting_from_env("database_url")
+        config.include("checkmate.models")
+        config.include("checkmate.db")
+
+        # Use pyramid_tm's explicit transaction manager.
+        config.include("pyramid_tm")
+
+        # This means that trying to access a request's transaction after
+        # pyramid_tm has closed the request's transaction will crash, rather
+        # than implicitly opening a new transaction that doesn't get closed
+        # (and potentially leaking open DB connections).
+
+        # This is recommended in the pyramid_tm docs:
+        # https://docs.pylonsproject.org/projects/pyramid_tm/en/latest/#custom-transaction-managers
+        config.add_settings({"tm.manager_hook": pyramid_tm.explicit_manager})
+
+    def _configure_sentry(self, config):
         # Configure sentry
-        config.add_settings({"h_pyramid_sentry.celery_support": True})
+        config.add_settings(
+            {
+                "h_pyramid_sentry.filters": [],
+                "h_pyramid_sentry.sqlalchemy_support": True,
+            }
+        )
+        if self.celery_worker:
+            config.add_settings({"h_pyramid_sentry.celery_support": True})
 
-    else:
-        # The celery workers don't need to know about this stuff
-        config.include("pyramid_jinja2")
-        config.include("pyramid_services")
-
-        # Include the secret for crytographic functions
-        config.registry.settings["checkmate_secret"] = os.environ["CHECKMATE_SECRET"]
-
-        config.scan("checkmate.views")
-        config.include("checkmate.routes")
-        config.include("checkmate.services")
-
-    # Configure sentry
-    config.add_settings(
-        {
-            "h_pyramid_sentry.sqlalchemy_support": True,
-            # If we enable Pyramid retries
-            # "h_pyramid_sentry.retry_support": True,
-        }
-    )
-    config.include("h_pyramid_sentry")
-
-    config.include("checkmate.models")
-    config.include("checkmate.db")
+        config.include("h_pyramid_sentry")
 
 
 def create_app(_=None, celery_worker=False, **settings):  # pragma: no cover
     """Configure and return the WSGI app."""
 
-    settings = load_settings(settings, celery_worker)
     config = pyramid.config.Configurator(settings=settings)
-
-    configure(config, celery_worker=celery_worker)
-
-    # pyramid-sanity should be activated as late as possible
-    config.include("pyramid_sanity")
+    CheckmateConfigurator(config=config, celery_worker=celery_worker)
 
     return config.make_wsgi_app()
