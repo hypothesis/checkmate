81957645f2947d93e6d714169fe44d7e00c7b015
Sean Hammond <seanh@users.noreply.github.com>
2021-03-23 17:55:17 +0000

    Add functional tests for the add-to-allow-list API

---
+++ tests/functional/checkmate/views/ui/api/add_to_allow_list_test.py
@@ -0,0 +1,38 @@
+import pytest
+
+
+class TestAddToAllowList:
+    def test_it_404s_if_theres_no_authentication(self, app, json_body):
+        app.post_json("/ui/api/rule", json_body, status=404)
+
+    def test_a_request_with_http_basic_auth_is_not_authenticated(self, app, json_body):
+        app.authorization = ("Basic", ("dev_api_key", ""))
+
+        app.post_json("/ui/api/rule", json_body, status=404)
+
+    @pytest.mark.usefixtures("logged_in")
+    def test_a_request_with_a_session_cookie_succeeds(self, app, json_body):
+        response = app.post_json("/ui/api/rule", json_body, status=200)
+
+        assert response.json == {
+            "data": {
+                "attributes": {
+                    "force": False,
+                    "hash": "73d986e009065f182c10bcb6a45db3d6eda9498f8930654af2653f8a938cd801",
+                    "rule": "example.com/",
+                    "tags": ["manual"],
+                },
+                "id": 1,
+                "type": "AllowRule",
+            }
+        }
+
+    @pytest.fixture
+    def json_body(self):
+        """Return a valid JSON body for POSTing to the API."""
+        return {
+            "data": {
+                "type": "AllowRule",
+                "attributes": {"url": "http://example.com"},
+            }
+        }
