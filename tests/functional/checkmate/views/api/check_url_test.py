fac13a7d621a4ec60971156ad55b19bd20a46396
Sean Hammond <seanh@users.noreply.github.com>
2021-04-01 18:44:43 +0100

    Tidy up some tests
    Just making these a little more concise

--- tests/functional/checkmate/views/api/check_url_test.py
+++ tests/functional/checkmate/views/api/check_url_test.py
@@ -5,27 +5,19 @@ from checkmate.models import BlockedFor
 
 class TestAPIAuth:
     def test_403_forbidden(self, app):
-        res = app.get("/api/check", expect_errors=True)
-
-        assert res.status_code == 403
+        app.get("/api/check", status=403)
 
     def test_403_forbidden_no_matching_users(self, app):
         app.authorization = ("Basic", ("api_nonsense",))
 
-        res = app.get("/api/check", expect_errors=True)
-
-        assert res.status_code == 403
+        app.get("/api/check", status=403)
 
     def test_authenticated_existing_user(self, app, pyramid_settings):
         checked_url = "http://example.com"
         app.authorization = ("Basic", ("dev_api_key", ""))
 
-        res = app.get(
-            "/api/check",
-            {"url": checked_url},
-        )
+        res = app.get("/api/check", {"url": checked_url}, status=200)
 
-        assert res.status_code == 200
         assert res.json["data"][0]["id"] == "not-explicitly-allowed"
         assert res.json["links"]["html"] == Any.url(
             scheme=pyramid_settings["public_scheme"],
