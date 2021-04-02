6e085ce45c1390dac27b2223fcba6a7c0c130ff3
Jon Betts <jon.betts@hypothes.is>
2021-04-02 19:40:00 +0100

    Prevent caching of the status end-point

--- checkmate/views/status.py
+++ checkmate/views/status.py
@@ -3,7 +3,7 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name="get_status", renderer="json")
+@view_config(route_name="get_status", renderer="json", http_cache=0)
 def get_status(_request):
     """Status endpoint."""
 
