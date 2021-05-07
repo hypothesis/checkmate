4923c7c2c69e572beee6e7903e313c2966585b18
Jon Betts <jon.betts@hypothes.is>
2021-05-07 11:47:02 +0100

    Remove setup.py indirection for DB related actions
    We do a number of things to avoid just calling a script normally:
    
     * Have a setup.py
     * Install the package in the dev env
     * This now forces us to compile the dependencies as well
    
    This results in about a 6 second delay when starting fresh, and mostly just makes how this works more confusing.

---
+++ bin/update_dev_data.py
@@ -0,0 +1,58 @@
+"""Setup some basic local data to test with."""
+
+import os
+import shutil
+import subprocess
+import sys
+from tempfile import TemporaryDirectory
+
+from pkg_resources import resource_filename
+from pyramid.paster import bootstrap
+
+from checkmate.checker.url.custom_rules import CustomRules
+from checkmate.models import Reason
+
+
+def update_remote_dev_data():
+    """Get secrets and data stored in the devdata repo."""
+
+    with TemporaryDirectory() as git_dir:
+        subprocess.check_call(
+            [
+                "git",
+                "clone",
+                "git@github.com:hypothesis/devdata.git",
+                git_dir,
+                # Truncate the git history, we just want the files
+                "--depth=1",
+            ]
+        )
+
+        # Copy devdata env file into place.
+        shutil.copyfile(
+            os.path.join(git_dir, "checkmate", "devdata.env"),
+            resource_filename("checkmate", "../.devdata.env"),
+        )
+
+
+def update_dev_data():
+    """Create some usable data to run against in dev."""
+
+    raw_rules = {
+        "example.org": Reason.PUBLISHER_BLOCKED,
+        "example.net": Reason.MEDIA_VIDEO,
+        "bad.example.com": Reason.MALICIOUS,
+    }
+    CustomRules(request.db).load_simple_rules(raw_rules)
+    print(f"Loaded {len(raw_rules)} custom rules")
+
+
+if __name__ == "__main__":
+    update_remote_dev_data()
+
+    config_file = sys.argv[1]
+    with bootstrap(config_file) as env:
+        request = env["request"]
+
+        with request.tm:
+            update_dev_data()
