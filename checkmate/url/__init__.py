11a4fc43a38a2c22b286f38252d0d0635154707b
Jon Betts <jon.betts@hypothes.is>
2022-04-20 13:11:46 +0100

    Remove URL related code

--- checkmate/url/__init__.py
+++
@@ -1,9 +0,0 @@
-"""An implementation of Google Web Risk normalisation routines.
-
-For more details see: https://cloud.google.com/web-risk/docs/urls-hashing
-"""
-
-from checkmate.url.canonicalize import CanonicalURL
-from checkmate.url.domain import Domain
-from checkmate.url.expand import ExpandURL
-from checkmate.url.hash import hash_for_rule, hash_url
