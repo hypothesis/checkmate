b59652ad6976e7e048fabe1daf0f4c11b50a9a33
Jon Betts <jon.betts@hypothes.is>
2022-04-20 13:11:46 +0100

    Remove URL related code

--- checkmate/url/domain/__init__.py
+++
@@ -1,4 +0,0 @@
-from checkmate.url.domain.domain import Domain
-from checkmate.url.domain.enums import SuffixType
-from checkmate.url.domain.public_suffix import PublicSuffix
-from checkmate.url.domain.top_level_domain import TopLevelDomain
--- checkmate/url/domain/_data.py
+++
@@ -1,9 +0,0 @@
-import importlib_resources
-
-
-def load_data(filename):
-    file_ref = importlib_resources.files("checkmate") / filename
-
-    with importlib_resources.as_file(file_ref):
-        with open(file_ref, encoding="utf8") as handle:
-            return [line.strip() for line in handle]
