3cc6c2ca72c86c2b1388547244d760f40dc2220e
Sean Hammond <seanh@users.noreply.github.com>
2024-01-31 12:05:55 +0000

    Add Sentry support to status endpoint

--- checkmate/views/status.py
+++ checkmate/views/status.py
@@ -1,10 +1,10 @@
-"""The status end-point required to allow us to work with AWS."""
-
 from pyramid.view import view_config
+from sentry_sdk import capture_message
 
 
-@view_config(route_name="get_status", renderer="json", http_cache=0)
-def get_status(_request):
-    """Status endpoint."""
+@view_config(route_name="status", renderer="json", http_cache=0)
+def status(request):
+    if "sentry" in request.params:
+        capture_message("Test message from Checkmate's status view")
 
     return {"status": "okay"}
--- tests/unit/checkmate/views/status_test.py
+++ tests/unit/checkmate/views/status_test.py
@@ -1,8 +1,24 @@
+import pytest
 from h_matchers import Any
 
-from checkmate.views.status import get_status
+from checkmate.views.status import status
 
 
 class TestStatusRoute:
-    def test_status_returns_a_response(self, pyramid_request):
-        assert get_status(pyramid_request) == Any.dict()
+    def test_it(self, pyramid_request, capture_message):
+        assert status(pyramid_request) == Any.dict()
+        capture_message.assert_not_called()
+
+    def test_it_sends_test_messages_to_sentry(self, pyramid_request, capture_message):
+        pyramid_request.params["sentry"] = ""
+
+        status(pyramid_request)
+
+        capture_message.assert_called_once_with(
+            "Test message from Checkmate's status view"
+        )
+
+
+@pytest.fixture(autouse=True)
+def capture_message(patch):
+    return patch("checkmate.views.status.capture_message")
