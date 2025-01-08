d3ef36d9d4aa6403609c551d8c4655d8de1f5b63
Misha Tomilov <mtomilov@gmail.com>
2025-01-08 13:03:17 +0100

    Remove sync blocklist (#945)

--- checkmate/celery_async/tasks.py
+++ checkmate/celery_async/tasks.py
@@ -1,37 +1,12 @@
 """Async tasks."""
 
 from celery.utils.log import get_task_logger
-from requests import RequestException
 
 from checkmate.celery_async.celery import app
-from checkmate.checker.url import CustomRules, URLHaus
+from checkmate.checker.url import URLHaus
 from checkmate.exceptions import StageRetryableException
 
 LOG = get_task_logger(__name__)
-
-
-@app.task
-def sync_blocklist():
-    """Download the online version of the blocklist."""
-
-    # pylint: disable=no-member
-    # PyLint doesn't know about the `request_context` method that we add
-    with app.request_context() as request:
-        url = request.registry.settings["checkmate_blocklist_url"]
-        if not url:
-            LOG.warning("Not updating blocklist as no URL is present")
-            return
-
-        LOG.info("Updating blocklist from '%s'", url)
-
-        with request.tm:
-            try:
-                raw_rules = CustomRules(request.db).load_simple_rule_url(url)
-            except RequestException as err:
-                LOG.exception("Could not update blocklist: %s", err)
-                return
-
-        LOG.info("Updated %s custom rules", len(raw_rules))
 
 
 def pipeline_task(wrapped_function):
